b3cf49040fa109f3f75ccbe40354fc193967dd07
pynoor <32771762+pynoor@users.noreply.github.com>
2018-11-28 13:53:26 +0100

    Render template to include forms

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from app import app
+from app.forms import WordForm
+
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    return "Let's get to work."
+    form = WordForm()
+    return render_template('index.html', title='Word Count Application', form=form)
