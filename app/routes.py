7e5270826aa4c622d70b91035ba903f6fdfa05ba
pynoor <32771762+pynoor@users.noreply.github.com>
2018-11-28 15:28:34 +0100

    feat: web scraper

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,29 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import WordForm
+import requests
+import obo
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Word Count Application')
+    return render_template('index.html')
 
 
 @app.route('/count', methods=['GET', 'POST'])
 def count():
     form = WordForm()
     if form.validate_on_submit():
-        flash('Counting the word {}.'.format(form.word.data))
-        return redirect('/index')
+        url = form.url.data
+        response = requests.get(url)
+        html = response.content.decode("utf-8")
+        text = obo.stripTags(html).lower()
+        fullwordlist = obo.stripNonAlphaNum(text)
+        wordlist = obo.removeStopwords(fullwordlist, obo.stopwords)
+        dictionary = obo.wordListToFreqDict(wordlist)
+        sorteddict = obo.sortFreqDict(dictionary)
+        for s in sorteddict[:21]:
+            flash(str(s))
+        return redirect(url_for('index'))
     return render_template('count.html', title='Word Count Application', form=form)
